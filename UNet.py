a184bf138817d5f792fce2cde92a54e17bffff48
권태확 <tylorkwon@gmail.com>
2021-03-29 00:06:46 +0900

    UNet

---
+++ UNet.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1 ,bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    
+    def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
+        super(UNet, self).__init__()
+        self.downs = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.pool = nn.MaxPool2d(2, 2)
+
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+
+        for feature in features[::-1]:
+            self.ups.append(
+                nn.ConvTranspose2d(2*feature, feature, 2, 2)
+                )
+            self.ups.append(DoubleConv(2*feature, feature))
+        
+        self.final_conv = nn.Conv2d(features[0], out_channels, 1)
+
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+        
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
+            if x.shape != skip_connection.shape:
+                x = transforms.functional.center_crop(x, output_size=skip_connection.shape[-2:])
+            concat = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx+1](concat)
+        return self.final_conv(x)
